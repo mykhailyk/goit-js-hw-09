893369fb27c8eaa4a73f1e4cc4ba4f60e89a7377
Yurii Mikhaylik <imxomunn@gmail.com>
2022-10-07 13:20:42 +0300

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+
+startBtn.addEventListener('click', startHandler);
+stopBtn.addEventListener('click', stopHandler);
+
+function changeColor() {
+  body.style.backgroundColor = `${getRandomHexColor()}`;
+}
+
+function startHandler() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+
+  changeColor();
+  timerId = setInterval(changeColor, 1000);
+}
+
+function stopHandler() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(timerId);
+}
